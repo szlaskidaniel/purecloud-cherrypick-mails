d88dba0503caa3cb74c702912b9f882202e3248b
Daniel Szlaski <szlaski.daniel@gmail.com>
2019-05-23 12:59:47 +0200

    new redirect

--- scripts/global.js
+++ scripts/global.js
@@ -1,5 +1,5 @@
 'use strict';
 
-var globalRedirectUrl = 'http://localhost:9000/';
+var globalRedirectUrl = 'https://szlaskidaniel.github.io/purecloud-cherrypick-mails/#/';
 var globalEnvironment = 'mypurecloud.ie';
 var globalClientId = 'ff35b816-7ee1-412c-8ae6-a70461e36903';
